1b23c013ba5dfcfa3a41317cb66a73727627173b
LarsAssen <juniorassen@hotmail.com>
2023-02-19 17:36:07 +1300

    added tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       </main>
     </>
   )
