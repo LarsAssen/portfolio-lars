3f58c732ba6aed344e4a1b544dbe79175cb1229c
LarsAssen <juniorassen@hotmail.com>
2023-04-07 09:41:39 +1200

    fixed style buttons

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -6,13 +6,13 @@ import { ComponentType } from "@/Enums/componentType"
 const Button:React.FC<{model: ButtonModel}> = ({model}) => {
   var style = ""
   if(model.type === ComponentType.Primary){
-    style = "bg-primary text-white font-bold py-2 px-4 rounded cursor-not-allowed"
+    style = "bg-primary text-white py-2 px-6 rounded font-bold cursor-not-allowed border-2 border-primary"
   }else if(model.type === ComponentType.Secondary){
-    style = "bg-secondary text-white font-bold py-2 px-4 rounded cursor-not-allowed"
+    style = "text-primary border-2 border-primary font-bold py-2 px-6 rounded cursor-not-allowed"
   }
 
   return (
-    <Link href={model.link}><div className={style}>{model.text}</div></Link>
+    <Link href={model.link}><button className={style}>{model.text}</button></Link>
   )
 }
 
