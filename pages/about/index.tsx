f45ecd992b38cd9080fcd774c45539b1c892fcc1
LarsAssen <juniorassen@hotmail.com>
2023-05-09 21:17:03 +1200

    styling fix aboutpage

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,21 +1,28 @@
 import Title from '@/components/atoms/Title'
 import React from 'react'
+import Image from 'next/image'
 
 const index:React.FC<{}> = ({}) => {
+  const personalImage = "/lars.png"
   return (
     <div className='h-screen'>
       <div className='mx-20 top-52 relative flex flex-row items-center '>
-      <Title text="About me" />
+        <div>
+        <Title text="About me" />
       <p className='my-2 text-white'>Hi there, my name is Lars and
          I really enjoy building websites 
          and web apps with the latest technologies. 
          In my free time I enjoy running, reading, and working out.
       </p>
-      <p>
+      <p className='my-2 text-white'>
          After travelling around New Zealand in a small
           camper with my wife, we decided to settle down in
            Christchurch and start our life here. 
       </p>
+        </div>
+        <div>
+        <Image src={personalImage} alt="Hero" width={500} height={500} />
+        </div>
       </div>  
     </div>
   )
