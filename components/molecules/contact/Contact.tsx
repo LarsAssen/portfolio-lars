a7aa0086038395dcb712c489d2c69f7215da4526
LarsAssen <juniorassen@hotmail.com>
2023-08-10 17:21:19 +1200

    fixed contact css

--- components/molecules/contact/Contact.tsx
+++ components/molecules/contact/Contact.tsx
@@ -38,32 +38,127 @@ const Contact = () => {
       [name]: value,
     }));
   };
+
+  const [fullnameFocused, setFullnameFocused] = useState(false);
+  const [emailFocused, setEmailFocused] = useState(false);
+  const [textareaFocused, setTextareaFocused] = useState(false);
+
+  const handleInput1Focus = () => {
+    setFullnameFocused(true);
+  };
+
+  const handleInput1Blur = () => {
+    setFullnameFocused(false);
+  };
+
+  const handleInput2Focus = () => {
+    setEmailFocused(true);
+  };
+
+  const handleInput2Blur = () => {
+    setEmailFocused(false);
+  };
+
+  const handleTextareaFocus = () => {
+    setTextareaFocused(true);
+  };
+
+  const handleTextareaBlur = () => {
+    setTextareaFocused(false);
+  };
+
+
+  
     
   
   return (
-    <div id='contact' className="bg-cardBg mx-20 mt-24">
-      <h2 className="text-2xl text-white font-bold mb-4">Like what you see?</h2>
-      <h2 className="text-2xl text-primary font-bold mb-4">Lets talk!</h2>
+    <div id='contact' className="bg-cardBg mx-20 my-24">
+      <h2 className="text-2xl pt-10 ml-10 font-light text-white mb-1">Like what you see?</h2>
+      <h2 className="text-2xl ml-10 font-light text-primary mb-4">Lets talk!</h2>
+      
+      
       <form onSubmit={handleSubmit}>
-        <div className="mb-4">
+        {/* <div className="mb-4">
           <label htmlFor="full-name">Full Name:</label>
           <input type="text" name="name" value={formData.name} onChange={handleChange} />
         </div>
         <div className="mb-4">
           <label htmlFor="email">Email:</label>
           <input type="email" name="email" value={formData.email} onChange={handleChange} />
+        </div> */}
+
+        <div className="flex mx-10 space-x-4 mb-4">
+          <div className={`relative  flex-1`}>
+            <label
+              className={`absolute left-2 transition-all duration-300 ${
+                fullnameFocused ? 'text-sm top-0 text-gray-500' : 'top-2'
+              }`}
+            >
+              Full Name
+            </label>
+            <input
+              name="name" 
+              value={formData.name} 
+              onChange={handleChange}
+              type="text"
+              className={`w-full bg-[#3F4D5E] border px-3 py-2 rounded focus:outline-none focus:ring focus:border-blue-300 ${
+                fullnameFocused ? 'border-blue-500' : 'border-gray-300'
+              }`}
+              onFocus={handleInput1Focus}
+              onBlur={handleInput1Blur}
+            />
+          </div>
+          <div className={`relative flex-1`}>
+            <label
+              className={`absolute left-2 transition-all duration-300 ${
+                emailFocused ? 'text-sm top-0 text-gray-500' : 'top-2'
+              }`}
+            >
+              Email
+            </label>
+            <input
+              type="email" 
+              name="email" 
+              value={formData.email} 
+              onChange={handleChange}
+              className={`w-full border bg-[#3F4D5E] px-3 py-2 rounded focus:outline-none focus:ring focus:border-blue-300 ${
+                emailFocused ? 'border-blue-500' : 'border-gray-300'
+              }`}
+              onFocus={handleInput2Focus}
+              onBlur={handleInput2Blur}
+            />
+          </div>
         </div>
-        <div className="mb-4">
-          <label htmlFor="message">Message:</label>
-          <textarea name="message" value={formData.message} onChange={handleChange} />
+        <div className={`relative mx-10`}>
+          <label
+            className={`absolute text-white left-2 transition-all duration-300 ${
+              textareaFocused ? 'text-sm top-0 text-gray-500' : 'top-2'
+            }`}
+          >
+            Text Area
+          </label>
+          <textarea
+            className={`w-full bg-[#3F4D5E] px-3 py-2 rounded focus:outline-none ${
+              textareaFocused ? 'border-primary' : ''
+            }`}
+            onFocus={handleTextareaFocus}
+            onBlur={handleTextareaBlur}
+            name="message" 
+            value={formData.message} 
+            onChange={handleChange}
+          ></textarea>
         </div>
+
+
         <button
           type="submit"
-          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+          className="bg-blue-500 my-10  mx-10 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
         >
           Send Message
         </button>
       </form>
+
+
     </div>
   );
 };
